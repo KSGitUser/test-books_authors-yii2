29223c7f4cf55c07bd218e1fe403f96864729add
KSFirstUser <rusakk@mail.ru>
2019-02-09 00:49:48 +0300

    CRUDE

---
+++ models/tables/Authors.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "authors".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $surname
+ *
+ * @property Books[] $books
+ */
+class Authors extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'authors';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'surname'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'surname' => 'Surname',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooks()
+    {
+        return $this->hasMany(Books::className(), ['author_id' => 'id']);
+    }
+}
---
+++ models/tables/Books.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "books".
+ *
+ * @property int $id
+ * @property string $title
+ * @property int $author_id
+ *
+ * @property Authors $author
+ */
+class Books extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'books';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['author_id'], 'integer'],
+            [['title'], 'string', 'max' => 255],
+            [['author_id'], 'exist', 'skipOnError' => true, 'targetClass' => Authors::className(), 'targetAttribute' => ['author_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'author_id' => 'Author ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthor()
+    {
+        return $this->hasOne(Authors::className(), ['id' => 'author_id']);
+    }
+}
